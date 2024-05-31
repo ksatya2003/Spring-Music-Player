08fec1dcec313f5d620854a2249888eb2bf850ca
ksatya2003 <110590529+ksatya2003@users.noreply.github.com>
2024-05-31 15:24:57 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { CiSearch } from "react-icons/ci";
 import { IoHomeOutline } from "react-icons/io5";
 import { IoImages } from "react-icons/io5";
@@ -5,85 +6,55 @@ import { MdLibraryMusic } from "react-icons/md";
 import { GrUserManager } from "react-icons/gr";
 import { BsImages } from "react-icons/bs";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { FaHome } from 'react-icons/fa';
-import { FaSearch } from 'react-icons/fa';
-import './sidebar.css';
-const Sidebar = ({handleFocus}) => {
+import { AiOutlineHeart } from "react-icons/ai"; // Import the heart icon for favorites
+
+const Sidebar = ({ showFavorites, setShowFavorites }) => {
   return (
     <aside>
-
-      <div className="home">
-        <FaHome className="searchlogo" />
-        <p>      Home</p>
-      </div>
-      <div className="home">
-        <button onClick={handleFocus}>
-          <FaSearch className="searchlogo" />
-        </button>
+      <div className="searchbar">
+        <CiSearch fontSize={"25px"} /> 
+        <input type="search" placeholder='Search Song' /> 
       </div>
 
       <div className="option1">
         <b>MENU</b>
-
-
-
-        <a style={{ cursor: 'pointer' }}>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(false)}>
           <IoHomeOutline fontSize={"20px"} />
-          {/* <i className="fa fa-user-o" aria-hidden="true"></i> */}
           <p>My drive</p>
-
         </a>
-
-
-        <a style={{ cursor: 'pointer' }}>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(false)}>
           <IoImages fontSize={"20px"} />
           <p>Albums</p>
-
         </a>
-        <a style={{ cursor: 'pointer' }}>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(false)}>
           <MdLibraryMusic fontSize={"20px"} />
-          {/* <i className="fa fa-clone" aria-hidden="true"></i> */}
           <p>Music</p>
-
         </a>
-        <a style={{ cursor: 'pointer' }}>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(true)}>
+          <AiOutlineHeart fontSize={"20px"} />
+          <p>Favorites</p>
+        </a>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(false)}>
           <GrUserManager fontSize={"20px"} />
-          {/* <i className="fa fa-star-o" aria-hidden="true"></i> */}
           <p>Artist</p>
-
         </a>
-
       </div>
 
       <div className="option1">
         <b>PlayList</b>
-
-
-        <a style={{ cursor: 'pointer' }}>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(false)}>
           <BsImages fontSize={"20px"} />
-          {/* <i className="fa fa-user-o" aria-hidden="true"></i> */}
-          <p> playlist1</p>
-
-
+          <p>playlist1</p>
           <RiDeleteBin6Line className='delete' />
         </a>
-        <a style={{ cursor: 'pointer' }}>
+        <a href="javascript:void(0)" onClick={() => setShowFavorites(false)}>
           <BsImages fontSize={"20px"} />
-          {/* <i className="fa fa-user-o" aria-hidden="true"></i> */}
-          <p> playlist1</p>
-
-
+          <p>playlist2</p>
           <RiDeleteBin6Line className='delete' />
         </a>
-
-
-
-
       </div>
-
-
     </aside>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
